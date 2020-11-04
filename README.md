f058fc3720dbdef34aaaef8788d0114a16133428
john-paul.vistal <johnpaulvistal@gmail.com>
2020-11-04 10:32:00 +0800

    Updated Read me

--- README.md
+++ README.md
@@ -1,11 +1,62 @@
-# MongoDB Exercise project
-
-This project is an exercise to understand and handle HTTP Requests connected to MongoDB.
+# INVENTORY SYSTEM AND POS SYSTEM
 
 ## Set up the project
-
 ```
 npm install
+```
+
+```
+create mongoDB named inventory-system
+create three collections 
+    products
+    sales
+    users
+
+
+>>Insert this to users collection
+"username" : "sample",
+"password" : "p@ssw0rd",
+"level" : "admin",
+"fullname" : "Initial User",
+"email" : "sample@gmail.com",
+"gender" : "male",
+"birthDate" : "01-01-2000",
+"img" : "default1.jpg",
+"createdAt" : "01-01-2000",
+"updatedAt" : "01-01-2000"
+Add one user with admin user level before you delete the initial user
+
+>>Insert this to product Collection
+"img" : "shoes-3.png",
+"product_number" : 10000000,
+"product_name" : "Sample Product 1",
+"brand" : "Sample",
+"price" : 0,
+"qty" : 10,
+"created_at" : "11-3-2020 :: 14 : 0 : 39",
+"updated_at" : "11-3-2020 :: 14 : 0 : 39",
+"created_by" : "Initial User",
+"updated_by" : "Initial User"
+Add one product before you delete the initial product
+
+>>Insert this to sales
+ "products" : [{
+        "id" : 0,
+        "prodID" : "5f9966d4f85da70bc46363ed",
+        "productNumber" : 10000000,
+        "productName" : "Sample Product 1,
+        "price" : 0,
+        "qty" : 1,
+        "total" : 0}],
+    "dateRecorded" : "2020-11-03T11:49:04.796Z",
+    "invoiceNumber" : 1000000000,
+    "numberOfItems" : 1,
+    "total" : 0,
+    "cashTendered" : 0,
+    "change" : 0
+
+Add one transaction record before deleting the initial sales record.
+
 ```
 
 
--- public/js/script.js
+++ public/js/script.js
@@ -80,6 +80,47 @@ $(document).ready(function() {
 
     });
 
+    //SEARCH FOR Sales
+    $('#searchSales').on('keyup', function() {
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("searchSales");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("salesRecord");
+        tr = table.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    });
+
+    //SEARCH FOR Sales
+    $('#searchStock').on('keyup', function() {
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("searchStock");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("stockRecord");
+        tr = table.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[1];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    });
+
+
     //SEARCH FOR USERS
     $('#searchUser').on('keyup', function() {
         var input, filter, cards, cardContainer, name, i;
